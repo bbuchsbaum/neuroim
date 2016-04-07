949a2d8f3bd2dbc216b89b1bc16dc04b73e6a2b2
bbuchsbaum <brad.buchsbaum@gmail.com>
2016-04-07 13:32:26 -0400

    remove readr dependency

--- R/FREESURFER_IO.R
+++ R/FREESURFER_IO.R
@@ -4,15 +4,15 @@ readFreesurferAsciiHeader <- function(fileName) {
   list(vertices=ninfo[1], faces=ninfo[2], label=stripExtension(FREESURFER_ASCII_SURFACE_DSET, basename(fileName)), embedDimension=3, headerFile=fileName, dataFile=fileName)
 }
 
-#' @importFrom readr read_table
+# @importFrom readr read_table
 readFreesurferAsciiGeometry<- function(fileName) {
   if (!requireNamespace("rgl", quietly = TRUE)) {
     stop("Pkg needed for this function to work. Please install it.",
          call. = FALSE)
   }
   ninfo <- as.integer(strsplit(readLines(fileName, n=2)[2], " ")[[1]])
-  asctab <- read_table(fileName, skip=2)
-  
+  #asctab <- read_table(fileName, skip=2)
+  asctab <- read.table(fileName, skip=2)
   vertices <- as.matrix(asctab[1:ninfo[1],1:3])
   nodes <- as.matrix(asctab[(ninfo[1]+1):nrow(asctab),1:3])
   
